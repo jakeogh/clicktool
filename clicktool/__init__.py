5c623bfab384296c0c8ee33ca99fb8fa5ed04c13
jakeogh <github.com@v6y.net>
2022-05-03 14:51:50 -0700

    auto-commit

--- clicktool/__init__.py
+++ clicktool/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from .clicktool import click_add_options
 from .clicktool import click_arch_select
+from .clicktool import click_cookies
 from .clicktool import click_global_options
+from .clicktool import click_user_agent
 from .clicktool import tv
