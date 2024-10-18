8c2b8f72c53e39593c9e7300c6302a87915ee42a
jakeogh <github.com@v6y.net>
2024-10-18 00:04:45 -0700

    auto-commit

--- clicktool/__init__.py
+++ clicktool/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-from .clicktool import CONTEXT_SETTINGS
-from .clicktool import USER_AGENT
-from .clicktool import click_add_options
-from .clicktool import click_arch_select
-from .clicktool import click_cookies
-from .clicktool import click_global_options
-from .clicktool import click_user_agent
-from .clicktool import tv
-from .clicktool import tvicgvd
+"""
+isort:skip_file
+"""
+
+from .clicktool import CONTEXT_SETTINGS as CONTEXT_SETTINGS
+from .clicktool import USER_AGENT as USER_AGENT
+from .clicktool import click_add_options as click_add_options
+from .clicktool import click_arch_select as click_arch_select
+from .clicktool import click_cookies as click_cookies
+from .clicktool import click_global_options as click_global_options
+from .clicktool import click_user_agent as click_user_agent
+from .clicktool import tv as tv
+from .clicktool import tvicgvd as tvicgvd
